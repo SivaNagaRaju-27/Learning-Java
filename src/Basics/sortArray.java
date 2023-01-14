13ac4c1077c8a27ccc3bfca8176d19ce9803828f
SivaNagaRaju <tsnraju.edu@gmail.com>
2023-01-14 09:20:24 +0530

    Extending Array class with arrayFunctions Class

--- src/Basics/sortArray.java
+++ src/Basics/sortArray.java
@@ -1,6 +1,6 @@
 package Basics;
 import DataStructures.Array;
-public class sortArray {
+public class sortArray{
     public static void main(String args[]) {
         var init = new Array();
         init.array("Random");
--- src/DataStructures/Array.java
+++ src/DataStructures/Array.java
@@ -2,17 +2,17 @@ package DataStructures;
 import Basics.arrayFunctions;
 //import Sort.bubbleSort;
 
-public class Array {
-    arrayFunctions array = new arrayFunctions();
+public class Array extends arrayFunctions {
+    //arrayFunctions array = new arrayFunctions();
     int list[];
     //public static void main(String args[]) {
     public void array(String str){
         switch(str){
-            case "" : list = array.read();
-                    array.show(list, list.length);
+            case "" : list = read();
+                    show(list, list.length);
             break;
-            case "Random": list = array.readRandom();
-                    array.show(list, list.length);
+            case "Random": list = readRandom();
+                    show(list, list.length);
             break;
         }
     }
@@ -22,7 +22,7 @@ public class Array {
             case "BubbleSort" : var bubble = new sort.bubbleSort();
                     bubble.sort(list,list.length);
                     System.out.println("After Sorting...");
-                    array.show(list,list.length);
+                    show(list,list.length);
             break;
             default : System.out.println("There is No Such type of Sorting... as "+str);
         }
